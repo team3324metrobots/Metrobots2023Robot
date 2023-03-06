7f8b9520b5ce9df4a9226fc0390f8921d13abc58
SethGithubAcct <seth.adkins@protonmail.com>
2023-03-06 16:46:39 -0500

    Auto code??????????

---
+++ src/main/java/frc/team3324/robot/auto/GoToChargingStation.java
@@ -0,0 +1,29 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.team3324.robot.auto;
+
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.team3324.robot.drivetrain.commands.AutoBalance;
+import frc.team3324.robot.arm.Arm;
+import frc.team3324.robot.drivetrain.Drivetrain;
+import frc.team3324.robot.drivetrain.commands.TrajectoryDrive;
+import frc.team3324.robot.intake.Intake;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class GoToChargingStation extends SequentialCommandGroup {
+  /** Creates a new GoToChargingStation. */
+  public GoToChargingStation(Drivetrain drivetrain, Arm arm, Intake intake, Trajectory trajectory) {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand())
+
+    addCommands(
+      new TrajectoryDrive(drivetrain, trajectory),
+      new AutoBalance(drivetrain)
+    );
+  }
+}
