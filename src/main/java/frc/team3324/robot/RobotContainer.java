3f8b183b27a89b0501578afd66d5c7bf16e3e8e6
Mateo-Johnson <johnson.12@themetroschool.org>
2023-03-09 16:10:22 -0500

    Added vision trigger

--- src/main/java/frc/team3324/robot/RobotContainer.java
+++ src/main/java/frc/team3324/robot/RobotContainer.java
@@ -15,6 +15,8 @@ import frc.team3324.robot.drivetrain.commands.GyroTurn;
 import frc.team3324.robot.intake.Intake;
 import frc.team3324.robot.intake.commands.IntakeCone;
 import frc.team3324.robot.intake.commands.IntakeCube;
+import frc.team3324.robot.vision.Vision;
+import frc.team3324.robot.vision.commands.AlignWithVision;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -36,6 +38,7 @@ public class RobotContainer {
   private static Arm arm = new Arm();
   private static Drivetrain drivetrain = new Drivetrain();
   private static Intake intake = new Intake();
+  private static Vision vision = new Vision();
 
   private NetworkTableInstance nt_instance = NetworkTableInstance.getDefault();
 
@@ -50,6 +53,8 @@ public class RobotContainer {
     drivetrain.setDefaultCommand(new Drive(drivetrain, primaryDriver::getLeftTriggerAxis, primaryDriver::getRightTriggerAxis, primaryDriver::getLeftX));
     arm.setDefaultCommand(new ControlArm(arm, secondaryDriver::getLeftY));
     primaryDriver.y().whileTrue(new GyroTurn(drivetrain, 90));
+
+    primaryDriver.b().whileTrue(new AlignWithVision(vision, drivetrain));
     // primaryDriver.rightBumper().whileTrue(new IntakeCone(intake, 0.1));
     // primaryDriver.leftBumper().whileTrue(new IntakeCone(intake, -0.1));
     secondaryDriver.rightTrigger().whileTrue(new IntakeCone(intake, 0.2));
--- src/main/java/frc/team3324/robot/vision/commands/AlignWithVision.java
+++ src/main/java/frc/team3324/robot/vision/commands/AlignWithVision.java
@@ -5,28 +5,36 @@
 package frc.team3324.robot.vision.commands;
 
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.team3324.robot.drivetrain.Drivetrain;
+import frc.team3324.robot.vision.Vision;
 
 public class AlignWithVision extends CommandBase {
+  Vision vision;
   Drivetrain drivetrain;
 
   /** Creates a new AlignWithVision. */
-  public AlignWithVision(Drivetrain drivetrain) {
+  public AlignWithVision(Vision vision, Drivetrain drivetrain) {
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(drivetrain);
+    addRequirements(vision, drivetrain);
     this.drivetrain = drivetrain;
+    this.vision = vision;
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("camMode").setNumber(0);
     NetworkTableInstance.getDefault().getTable("limelight").getEntry("pipeline").setNumber(1);
+
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+
+    SmartDashboard.putNumber("Target ID", NetworkTableInstance.getDefault().getTable("limelight").getEntry("tid").getDouble(0));
 
     float Kp = -0.1f;  // Proportional control constant
     double tx = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0.0);
